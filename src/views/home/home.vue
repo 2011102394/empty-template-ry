5ae2205497c84ae83c0735ee17b9412df994a999
zhangcj <602173141@qq.com>
2023-01-12 15:59:43 +0800

    feat: 引入vue-router 并配置

---
+++ src/views/home/home.vue
@@ -0,0 +1,17 @@
+<!--
+    @description home页面
+    @author zhangcj
+    @date 2023-01-12 15:52:19
+ -->
+<template>
+  <div class="home-container">home</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.home-container {
+  height: 100%;
+  width: 100%;
+}
+</style>
