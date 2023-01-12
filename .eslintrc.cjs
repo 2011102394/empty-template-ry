5ded4666654564fa61db5b073c991fbec1fa67c9
zhangcj <602173141@qq.com>
2023-01-12 15:30:10 +0800

    feat: 添加prettier 并格式化代码

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
-  parser: "vue-eslint-parser",
+  // 指定如何解析语法。可以为空，但若不为空，只能配该值
+  parser: 'vue-eslint-parser',
   extends: [
-    "eslint:recommended",
-    "plugin:vue/essential",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  plugins: ["vue", "@typescript-eslint"],
-  rules: {},
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'vue/no-multiple-template-root': 0,
+    'prettier/prettier': 'error',
+    // 声明未使用的变量
+    '@typescript-eslint/no-unused-vars': 1,
+    // 组件名不能是单个单词
+    'vue/multi-word-component-names': 0,
+    'no-undef': 0,
+    '@typescript-eslint/no-explicit-any': ['off']
+  }
 }
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, // 是否使用分号
+  singleQuote: true, // 使用单引号代替双引号
+  trailingComma: 'none', // 多行时尽可能使用逗号结尾
+  endOfLine: 'auto',
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'ignore'
+}
