d29bea746a6015ede29cbeeffc2f93676fe3d9b6
zhangcj <602173141@qq.com>
2023-01-12 16:14:46 +0800

    feat: 默认使用思源黑体字体

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import '@/styles/index.scss'
 import router from '@/router/index'
 import App from './App.vue'
-
+import '@/assets/fonts/font.css'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
