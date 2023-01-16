130aabd2a3a45ac643e8e4b51a88a7fddd04c498
zhangcj <602173141@qq.com>
2023-01-16 09:48:53 +0800

    feat: 新增form表单组件

---
+++ src/components/ui/form/src/index.vue
@@ -0,0 +1,15 @@
+<!--
+    @description form表单封装
+    @author zhangcj
+    @date 2023-01-16 09:45:41
+ -->
+<template>
+  <div class="arsc-form-container">form</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.arsc-form-container {
+}
+</style>
